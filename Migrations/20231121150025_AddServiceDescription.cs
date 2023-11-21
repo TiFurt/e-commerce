16144cc44c5929823ae7b15bc9d252d4abde6005
Thiago Cardoso <thiagoc1415@gmail.com>
2023-11-21 19:02:12 -0300

    feat: adicionando migrations

---
+++ Migrations/20231121150025_AddServiceDescription.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace e_commerce.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddServiceDescription : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Ammount",
+                table: "Services");
+
+            migrationBuilder.RenameColumn(
+                name: "Type",
+                table: "Services",
+                newName: "Description");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Description",
+                table: "Services",
+                newName: "Type");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Ammount",
+                table: "Services",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
